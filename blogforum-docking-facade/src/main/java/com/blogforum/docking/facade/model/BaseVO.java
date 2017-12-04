8fd8d5a2e65fff1b28f5039c1ec60e7dd38349c5
MrNiceWangwendi <897136043@qq.com>
2017-12-05 01:31:05 +0800

    创建

---
+++ blogforum-docking-facade/src/main/java/com/blogforum/docking/facade/model/BaseVO.java
@@ -0,0 +1,12 @@
+package com.blogforum.docking.facade.model;
+
+import java.io.Serializable;
+
+public class BaseVO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -653442829148294530L;
+
+}
